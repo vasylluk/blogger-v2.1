00151a41651263779e92ca860493bbf5d59a55ff
vasylluk <32761553+vasylluk@users.noreply.github.com>
2017-11-06 12:26:33 +0200

    nickname validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :nickname, :password, :email, presence: true
+  validates :nickname, length: { in: 5..15 }
+  validates :nickname, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
